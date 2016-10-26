b874b9c0de32f2b4377f79e03a532423cbf41cae
Saurabh Datar <sdatar2@stanford.edu>
2016-10-25 22:07:36 -0400

    Big JSON with leaflet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,14 @@
-// var map = L.map('map', {
-//   zoom: 12,
-//   center: [18.9920833,72.8636392]
-// });
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3NkYXRhciIsImEiOiJjaW0wMTh3a3owOGlldTBrc3IxeDN6b2htIn0.iXmePtlBTCRB6JM7q-xDcg';
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v9',
-    center: [18.9920833,72.8636392].reverse(),
-    zoom: 12
+var map = L.map('map', {
+  zoom: 12,
+  center: [18.9920833,72.8636392]
 });
 
-// L.mapbox.accessToken = 'pk.eyJ1Ijoic3NkYXRhciIsImEiOiJjaW0wMTh3a3owOGlldTBrc3IxeDN6b2htIn0.iXmePtlBTCRB6JM7q-xDcg';
 
-// var map = L.mapbox.map('map', 'mapbox.light').setView([18.9920833,72.8636392], 12),
-//     layer = L.geoJson(null, { style: { color: '#333', weight: 1 }});
-
-// map.addLayer(layer);
-
-// L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', 
-//     {
-//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-//   maxZoom: 18
-// }).addTo(map);
+L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', 
+    {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+  maxZoom: 18
+}).addTo(map);
 
 // L.TopoJSON = L.GeoJSON.extend({  
 //   addData: function(jsonData) {    
@@ -43,8 +28,7 @@ var map = new mapboxgl.Map({
 // var topoLayer = new L.TopoJSON();
 
 $.getJSON('data/mumbai.json')
-.done(addTopoData); 
-
+.done(geoData);
 
 function geoData(data) {
   var geoJson = L.geoJSON(data, {
@@ -68,106 +52,4 @@ function onEachFeature(feature, layer) {
 
 function style(feature, layer) {
   return { weight: 1};
-}
-
-
-
-function addTopoData(topoData){
-  // var streets = topojson.feature(topoData, topoData.objects.mumbai)
-  // layer.addData(streets);
-
-  map.on('load', function() {
-    map.addSource("road", {
-      type: "geojson",
-      data: topoData
-    });
-
-    map.addLayer({
-      "id": "road",
-      "type": "line",
-      "source": "road",
-      "layout": {
-          "line-join": "round",
-          "line-cap": "round"
-      },
-      "paint": {
-          "line-color": "#888",
-          "line-width": 1.2
-      }
-    });
-
-    map.on('click', function (e) {
-      var features = map.queryRenderedFeatures(e.point, { layers: ['road'] });
-      if (!features.length) {
-        return;
-      }
-
-      var feature = features[0];
-      var coordinates = feature.geometry.coordinates;
-
-      var popup = new mapboxgl.Popup()
-      .setLngLat(map.unproject(e.point))
-      .setHTML(feature.properties.name)
-      .addTo(map);
-
-      // Zoom to selected street
-      var bounds = coordinates.reduce(function(bounds, coord) {
-            return bounds.extend(coord);
-        }, new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]));
-
-      map.fitBounds(bounds, {
-        padding: 20
-      });
-    });
-
-    map.on('mousemove', function (e) {
-      var features = map.queryRenderedFeatures(e.point, { layers: ['road'] });
-      map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
-    });
-
-    map.on('zoom', function(e) {
-      if (map.getZoom() > 13) {
-        map.setPaintProperty('road', 'line-width', 3);
-      } else {
-        map.setPaintProperty('road', 'line-width', 1.2);
-      }
-    })
-
-  });
-  
-  // console.log(topoData);
-
-
-  // topoLayer.addData(topoData);
-  // topoLayer.addTo(map);
-
-  // topoLayer.eachLayer(function(layer) {
-  //   if (layer.feature.properties.name !== null) {
-  //     layer.setStyle({
-  //       weight: 1
-  //     });
-  //   } else {
-  //     layer.setStyle({
-  //       weight: 0
-  //     });
-  //   }
-    
-
-  //   layer.on({
-  //     mouseover: function() {
-  //       var tooltip = this.feature.properties.name;
-  //       layer.bindPopup('<p>Name: '+ tooltip + '</p>');
-  //     }
-  //   });
-  // });
-
-  // map.on('zoomend', function() {
-  //   if(map.getZoom() > 13) {
-  //     topoLayer.setStyle({weight: 3});
-  //   } else {
-  //     topoLayer.setStyle({ weight: 1});
-  //   }
-  // })
-
-
 }
