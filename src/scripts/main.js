cafc415c25749874a1bdb118ce0f8110efc13c11
Saurabh Datar <sdatar2@stanford.edu>
2017-03-26 21:51:26 -0400

    Got filtering working properly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,32 @@ var map = L.map('map', {
   center: [18.9920833,72.8636392]
 });
 
+var darkMap = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+  stamen = 'http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png';
 
-L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', 
+var mapLayerGroups = {};
+
+L.tileLayer(darkMap, 
     {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
   maxZoom: 18
 }).addTo(map);
 
 
-$.getJSON('data/mum-small.json').done(geoData);
+var streetLookup = {};
+
+$.getJSON('data/streets.json').done(geoData);
+
+var current = $('#categ').val();
+
+$('#categ').on('change', function() {
+  var selected = $('#categ').val();
+  showLayer(selected, current);
+});
+
+/******************************************
+******* FUNCTION THAT DRAWS THE MAP *******
+*******************************************/
 
 function geoData(data) {
   var geoJson = L.geoJSON(data, {
@@ -25,15 +42,57 @@ function geoData(data) {
     if(map.getZoom() > 13) {
       geoJson.setStyle({weight: 3});
     } else {
-      geoJson.setStyle({weight: 1});
+      geoJson.setStyle({weight: 1.5});
     }
   });
 }
 
 function onEachFeature(feature, layer) {
-  var tooltip = feature.properties.name;
-  layer.bindPopup('<p>Name: '+ tooltip + '</p>');
+  var tooltip = feature.properties.d;
+
+  streetLookup[feature.properties.osm_id] = layer;
+
+  layer.on('click', function(e) {
+    $('#info-box').html('<p>'+ feature.properties.n + '</p><p>' + tooltip + '</p>')
+  });
+
 }
 
 function style(feature, layer) {
+
+  if(feature.properties.g === 'm') {
+    return {
+      color: '#21abcd',
+      weight: 1.5
+    };
+  }
+
+  if (feature.properties.g === 'f') {
+    return {
+      color: '#cd5c5c',
+      weight: 1.5
+    };
+  }
+
+  return {
+      // opacity: 0,
+      color: '#fff',
+      weight: 1.5
+    };
+}
+
+
+/*
+* show/hide layerGroup   
+*/
+function showLayer(id) {
+
+  for (var i in streetLookup) {
+    if (streetLookup[i].feature.properties.c !== id && id !== 'all') {
+      map.removeLayer(streetLookup[i]);
+    } else {
+      map.addLayer(streetLookup[i]);
+    }
+  }
+
 }
