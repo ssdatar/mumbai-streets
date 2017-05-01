365fe13958b95f60190dfcc88cd82b96a8f38a1d
Saurabh Datar <sdatar2@stanford.edu>
2017-04-30 22:19:22 -0400

    Styling streets on mouseover, better styling for infobox, loading spinner added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,19 @@
 import * as L from 'leaflet';
+import {
+  event,
+  selection,
+  select,
+  selectAll
+} from "d3-selection";
 import { json } from 'd3-request';
 
+const isMobile = window.innerWidth < 600;
+const initZoom = (isMobile) ? 13 : 12;
 
 const map = L.map('map', {
-  zoom: 12,
-  center: [18.9920833,72.8636392]
+  zoom: initZoom,
+  center: [18.9920833,72.8284905079636]
+  //center: [18.9920833, 72.9195392]
 });
 
 var darkMap = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
@@ -15,6 +24,7 @@ var mapLayerGroups = {};
 L.tileLayer(darkMap, 
     {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+  minZoom: 12,
   maxZoom: 18
 }).addTo(map);
 
@@ -22,8 +32,9 @@ L.tileLayer(darkMap,
 var streetLookup = {};
 
 
-const categ = d3.select('#categ'),
-      infoBox = d3.select('#info-box');
+const categ = select('#categ'),
+      infoBox = select('#info-box'),
+      loading = select('.loading');
 
 const current = categ.property('value');
 
@@ -32,6 +43,7 @@ json('data/streets.json', geoData);
 
 categ.on('change', () => {
   var selected = categ.property('value');
+  map.setZoom(12);
   showLayer(selected, current);
 });
 
@@ -41,6 +53,8 @@ categ.on('change', () => {
 
 function geoData(data) {
 
+  loading.style('display', 'none');
+
   var geoJson = L.geoJSON(data, {
     style: style,
     onEachFeature: onEachFeature
@@ -48,12 +62,10 @@ function geoData(data) {
 
   geoJson.addTo(map);
 
-  map.on('zoomend', function() {
-    if(map.getZoom() > 13) {
-      geoJson.setStyle({weight: 3});
-    } else {
-      geoJson.setStyle({weight: 1.5});
-    }
+
+  map.on('zoomend', () => {
+    const weight = getWeight(map.getZoom());
+    geoJson.setStyle({weight: weight});
   });
 }
 
@@ -62,44 +74,77 @@ function geoData(data) {
 *******************************************/
 
 function onEachFeature(feature, layer) {
-  var tooltip = feature.properties.d;
+  var name = `<h4>${feature.properties.n}</h4>`;
+  var text = feature.properties.d;
+  var readMore = (feature.properties.l.length > 1) ?   
+                ` <a href="${feature.properties.l}" target="_blank">Read more...</a>` : 
+                '';
+
+  var current = this;
+
+  const tooltip = `<p>${text + readMore}</p>`;
 
   streetLookup[feature.properties.osm_id] = layer;
 
-  layer.on('click', function(e) {
-    infoBox.classed('show', true);
-    infoBox.html('<p>'+ feature.properties.n + '</p><p>' + tooltip + '</p>')
+  layer.on('click', function (e) {
+
+    // Highlight on click
+    this.setStyle({color: 'yellow'});
+
+    map.flyTo(e.latlng, map.getZoom());
+    
+    infoBox.classed('show', true)
+      .html(name + tooltip);
   });
 
+  // Highlight street on highlight
+  layer.on('mouseover', function(e) {
+    this.setStyle({color: 'yellow'});
+  })
+  
+
+  // Reset highlights
+  layer.on('mouseout', function(e) {
+    this.setStyle(style(this.feature))
+  })
 }
 
-function style(feature, layer) {
+/******************************************
+******* CLICK EVENT FUNCTION ON EACH FEATURE *******
+*******************************************/
 
-  if(feature.properties.g === 'm') {
-    return {
-      color: '#21abcd',
-      weight: 1.5
-    };
-  }
+function style(feature) {
 
-  if (feature.properties.g === 'f') {
-    return {
-      color: '#cd5c5c',
-      weight: 1.5
-    };
-  }
+  const colorLookup = {
+    'm': '#21abcd',
+    'f': '#cd5c5c',
+    'na': '#fff'
+  };
 
   return {
-      // opacity: 0,
-      color: '#fff',
-      weight: 1.5
-    };
+    color: colorLookup[feature.properties.g],
+    weight: getWeight(map.getZoom())
+  }
 }
 
 
-/*
-* show/hide layerGroup   
-*/
+function getWeight(zoom) {
+
+  console.log(zoom)
+  if (zoom > 15) {
+    return 7;
+  }
+
+  if (zoom > 13 && zoom < 15) {
+    return 5;
+  }
+
+  return 2;
+}
+
+/******************************************
+******* Show or hide streets based on what category is selected *******
+*******************************************/
 function showLayer(id) {
 
   for (var i in streetLookup) {
